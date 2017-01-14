220b99721035fd088962db9bb2e3eae6c6ae6f3a
Ivan <pain_s@mail.ru>
2017-01-14 22:13:18 +0200

    admin panel

--- build/admin/index.php
+++ build/admin/index.php
@@ -20,15 +20,16 @@ $mas = getArticle($route);
 
 		<section id="adminka" class="adaptive_container">
     <div id="add_remove" class="row">
-        <div class="add_remove_catalog_img col s6">
-          <p class="average_text col s11 m11 l11 offset-s1 offset-m1 offset-l1">Добавление каталога</p>
+       
+        <div class="add_remove_catalog_img col s12 m8 l6 offset-m2">
+          <p class="average_text col s10 m12 l11 offset-s1  offset-l1">Добавление каталога</p>
               <div class="row col s11 m11 l11 offset-s1 offset-m1 offset-l1 lines">
                 <div class="input-field col s6">
                   <input id="email" placeholder="Название каталога" type="text" class="validate average_text">
                 </div>
                 <button class="col s4 m4 l4 offset-s1 offset-m1 offset-l1 btn waves-effect waves-light average_text" type="submit" name="action">Добавить</button>
               </div>
-          <p class="average_text col s11 m11 l11 offset-s1 offset-m1 offset-l1">Удаление каталога</p>
+          <p class="average_text col s10 m12 l11 offset-s1  offset-l1">Удаление каталога</p>
            <div class="row col s11 m11 l11 offset-s1 offset-m1 offset-l1 lines">
                     <div class="input-field col s6">
                         <select  class="list average_text">
@@ -40,39 +41,115 @@ $mas = getArticle($route);
                   </div>
                 <button class="col s4 m4 l4 offset-s1 offset-m1 offset-l1 btn waves-effect waves-light average_text" type="submit" name="action">Удалить</button>
               </div>
-          
-          
-          <p class="average_text col s11 m11 l11 offset-s1 offset-m1 offset-l1">Добавление изображений</p>
-              <div class="row col s11 m11 l11 offset-s1 offset-m1 offset-l1 lines">
-                     
-                        <div class="file-field input-field row">
-                          
-                         
-                         
-                         
-                         
-                         
+          <p class="average_text col s10 m12 l11 offset-s1  offset-l1">Добавление изображений</p>
+              <div class="row col s10 m12 l11 offset-s1  offset-l1 down_block">
+                        <div class="file-field input-field">
                           <div class="btn">
-                            <p>File</p>
-                            <input type="file" multiple>
+                              <div class="out_bl"><span class="in_bl average_text">File</span></div>
+                            <input type="file" class="average_text" multiple>
                           </div>
-                          <div class="file-path-wrapper row">
-                            <input class="file-path validate col s12" type="text" placeholder="Upload one or more files">
+                          <div class="file-path-wrapper">
+                            <input class="file-path validate average_text" type="text" placeholder="1920x1080">
                           </div>
                         </div>
-                      
+                        <div class="file-field input-field">
+                          <div class="btn">
+                              <div class="out_bl"><span class="in_bl average_text">File</span></div>
+                            <input type="file" class="average_text" multiple>
+                          </div>
+                          <div class="file-path-wrapper">
+                            <input class="file-path validate average_text" type="text" placeholder="1024x768">
+                          </div>
+                        </div>
+                        <div class="file-field input-field">
+                          <div class="btn">
+                              <div class="out_bl"><span class="in_bl average_text">File</span></div>
+                            <input type="file" class="average_text" multiple>
+                          </div>
+                          <div class="file-path-wrapper">
+                            <input class="file-path validate average_text" type="text" placeholder="960x800">
+                          </div>
+                        </div>
               </div>
-          
-          
-          
+              <p class="average_text col s10 m12 l11 offset-s1  offset-l1">Удаление изображений</p>
+              <p class="average_text col s10 m12 l11 offset-s1  offset-l1 no_b">Выберите альбом и удалите изображение в отдельном окне</p>
+            <div class="row col s10 m12 l11 offset-s1  offset-l1 lines">
+                    <div class="input-field col s6">
+                        <select size="2" class="list average_text">
+                          <option class="average_text" value="" disabled selected>Выберите альбом </option>
+                          <option class="average_text" value="1">Option 1</option>
+                          <option class="average_text" value="2">Option 2</option>
+                          <option class="average_text" value="3">Option 3</option>
+                           <option class="average_text" value="1">Option 1</option>
+                          <option class="average_text" value="2">Option 2</option>
+                          <option class="average_text" value="3">Option 3</option>
+                           <option class="average_text" value="1">Option 1</option>
+                          <option class="average_text" value="2">Option 2</option>
+                          <option class="average_text" value="3">Option 3</option>
+                           <option class="average_text" value="1">Option 1</option>
+                          <option class="average_text" value="2">Option 2</option>
+                          <option class="average_text" value="3">Option 3</option>
+                        </select>
+                  </div>
+              </div>
+        </div>
+           
+            <div class="remove_image col s12 m12 l6">
+              <div class="row" id="imgContent">       
+                <div class="col s6 m4 l4 image_gallery">
+                    <div class="for_hight">
+                    <p class="remove_btn">×</p>
+                        <div class="for_hight2">
+                            <div class="img_wrap">
+                                <img class="img_block" src="../img/img/Material_Design_Wallpaper_WALLPAPERDNA (1).png">
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col s6 m4 l4 image_gallery">
+                    <div class="for_hight">
+                    <p class="remove_btn">×</p>
+                        <div class="for_hight2">
+                            <div class="img_wrap">
+                                <img class="img_block" src="../img/img/Material_Design_Wallpaper_WALLPAPERDNA (1).png">
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col s6 m4 l4 image_gallery">
+                    <div class="for_hight">
+                    <p class="remove_btn">×</p>
+                        <div class="for_hight2">
+                            <div class="img_wrap">
+                                <img class="img_block" src="../img/img/Material_Design_Wallpaper_WALLPAPERDNA (1).png">
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                   <div class="col s6 m4 l4 image_gallery">
+                    <div class="for_hight">
+                    <p class="remove_btn">×</p>
+                        <div class="for_hight2">
+                            <div class="img_wrap">
+                                <img class="img_block" src="../img/img/Material_Design_Wallpaper_WALLPAPERDNA (1).png">
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                   <div class="col s6 m4 l4 image_gallery">
+                    <div class="for_hight">
+                    <p class="remove_btn">×</p>
+                        <div class="for_hight2">
+                            <div class="img_wrap">
+                                <img class="img_block" src="../img/img/Material_Design_Wallpaper_WALLPAPERDNA (1).png">
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
         </div>
         
-        
-        
-        <div class="show_remove_image">
-            
-        </div>
-    </div>
     
     
     
