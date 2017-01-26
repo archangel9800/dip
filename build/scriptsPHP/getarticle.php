1a64971fa92deb485c2d5df89f426e1c8d4bbed2
Ivan <pain_s@mail.ru>
2017-01-26 20:47:00 +0200

    правка getarticle

--- build/scriptsPHP/getarticle.php
+++ build/scriptsPHP/getarticle.php
@@ -5,9 +5,11 @@ function getArticle($urlSegments){
     $myconnect = connectToDb();
     if (empty($urlSegments[0])) {
         $sql = "SELECT * FROM categories_db WHERE categories='main'" ;
+        $result = mysqli_query($myconnect, $sql);
     }
     else if($urlSegments[0] == 'admin'){
-        $sql = "SELECT * FROM categories_db WHERE categories='$urlSegments[1]'"; 
+        $sql = "SELECT * FROM categories_db WHERE categories='$urlSegments[1]'";
+        $result = mysqli_query($myconnect, $sql);
     }
     else{
         $sql = "SELECT * FROM categories_db WHERE categories='$urlSegments[0]'" ;
@@ -32,4 +34,51 @@ function getArticle($urlSegments){
     return $row;
 }
 //для адресной строки
+
+
+
+//
+//
+////для адресной строки
+//function getArticlee($urlSegments){
+//    //соединяюсь с базой
+////    echo $route;
+//    $myconnect = connectToDb();
+//    if (empty($urlSegments[0])) {
+//        $sql = "SELECT * FROM categories_db WHERE categories='main'" ;
+//    }else if($urlSegments[0] == 'admin'){
+//        $sql = "SELECT * FROM categories_db WHERE categories='$urlSegments[1]'"; 
+//    }else{
+//        $sql = "SELECT * FROM categories_db WHERE categories='$urlSegments[0]'" ;
+//    }
+//    $result = mysqli_query($myconnect, $sql);
+//    if (mysqli_num_rows($result) == 0){
+//        $sql = "SELECT * FROM categories_db WHERE categories='404' " ;
+//        $result = mysqli_query($myconnect, $sql);
+//    }
+//    // результат в ассоциативный массив
+//    $row = mysqli_fetch_assoc($result);
+//    closeConnectionToDb($myconnect);
+//    return $row;
+//}
+////для адресной строки
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
